a8e954e5227ab3ecbc42bc93430e816bfac451de
gautam yadav <gy77044@gmail.com>
2023-12-05 22:33:51 +0530

    largest number

--- Search/LinearSearch.java
+++ Search/LinearSearch.java
@@ -1,23 +1,44 @@
+// import java.util.*;
+
+// public class LinearSearch {
+//   public static int linSearch(int number[], int key) {
+//     for (int i = 0; i < number.length; i++) {
+//       if (number[i] == key) {
+//         return i;
+//       }
+//     }
+//     return -1;
+//   }
+
+//   public static void main(String args[]) {
+//     int numbers[] = { 2, 4, 5, 6, 8, 10, 12, 14, 16 };
+//     int key = 10;
+//     int index = linSearch(numbers, key);
+//     if (index == -1) {
+//       System.out.println("Not found.");
+//     } else {
+//       System.out.println("Key is at" + index);
+//     }
+//   }
+// }
+
+//Largest number
 import java.util.*;
 
 public class LinearSearch {
-  public static int linSearch(int number[], int key) {
-    for (int i = 0; i < number.length; i++) {
-      if (number[i] == key) {
-        return i;
+
+  public static int getLargest(int numbers[]) {
+    int largest = Integer.MIN_VALUE;
+    for (int i = 0; i < numbers.length; i++) {
+      if (largest < numbers[i]) {
+        largest = numbers[i];
       }
     }
-    return -1;
+    return largest;
   }
 
   public static void main(String args[]) {
     int numbers[] = { 2, 4, 5, 6, 8, 10, 12, 14, 16 };
-    int key = 10;
-    int index = linSearch(numbers, key);
-    if (index == -1) {
-      System.out.println("Not found.");
-    } else {
-      System.out.println("Key is at" + index);
-    }
+    System.out.println("largest value is : " + getLargest(numbers));
   }
 }
